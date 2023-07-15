502e2c0232d16a49656550c2dc4339182ccce5bb
ps299 <ps299106@gmail.com>
2023-07-15 09:56:31 +0530

    added hello world in index.js

--- myapp/backend/index.js
+++ myapp/backend/index.js
@@ -30,7 +30,9 @@ app.use("/auth", require("./routers/authRouter"));
 app.use("/user", require("./routers/userRouter"));
 app.use("/bank", require("./routers/bankRouter"));
 app.use("/camps", require("./routers/campRouter"));
-
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
 app.listen(process.env.PORT, () =>
 	console.log(`Server running at http://localhost:${process.env.PORT}`)
 );
