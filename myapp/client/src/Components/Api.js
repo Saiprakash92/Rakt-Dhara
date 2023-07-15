a9777a6bc09e2cb01f8dfc19b70ba70439bbbc70
ps299 <ps299106@gmail.com>
2023-07-15 10:15:02 +0530

    api update

--- myapp/client/src/Components/Api.js
+++ myapp/client/src/Components/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const baseURL = "https://rakt-dhara.vercel.app/";
+const baseURL = "https://rakt-dhara-w6cw.vercel.app";
 
 export default axios.create({ baseURL: baseURL });
